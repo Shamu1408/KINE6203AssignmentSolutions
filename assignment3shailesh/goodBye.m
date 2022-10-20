fa85221ae73523f692399ff6ede17c1cbdbaf576
Shamu1408 <gardass21@students.ecu.edu>
2022-10-20 15:22:13 -0400

    Create goodBye.m

---
+++ assignment3shailesh/goodBye.m
@@ -0,0 +1,7 @@
+function goodBye
+%this function displays the below mentioned message on calling it. The
+%function doesnt have an output or input.
+
+disp(" ")
+disp("Thank goodness this class is over!!");
+end
