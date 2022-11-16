9bc7a10fe38ed0b63099e9d42ae53bafeba75839
Shamu1408 <gardass21@students.ecu.edu>
2022-11-16 16:03:04 -0500

    Update guessThatNumber.m

--- guessThatNumber.m
+++ guessThatNumber.m
@@ -43,19 +43,17 @@ fprintf('   1) Beginner (range is 1 to %d)\n', beginnerHighest)
 fprintf('   2) Moderate (range is 1 to %d)\n', moderateHighest)
 fprintf('   3) Advanced (range is 1 to %d)\n', advancedHighest)
 
-level = input('Enter level (1-3): ','s'); 
+level = input('Enter level (1-3): '); 
 
 while level ~= beginner || level ~= moderate || level ~= advanced; 
-    %Bug No.1: An or (||) logical operator was used earlier. Using ||
-    % returns the game back to line 50 nd restarts the game. Whereas use
-    % of && operator, ends the game after the user guesses the correct
-    % number.
 fprintf('Sorry, that is not a valid level selection.\n');
 level = input('Please re-enter a level of play (1-3): ');
+%end Bug no.1: Commented the end from this line. This end resulted in the
+%code to be stuck in this infinite loop.
 
 % set highest secret number based on level selected
 if level == beginner 
-    %%Bug no. 2: I found this by trying to run the code and getting a 
+    %Bug no. 2: I found this by trying to run the code and getting a 
     % incorrect use of '=' error message. To determine equality we need to 
     % '==' instead of '='.
 highest = beginnerHighest;
@@ -63,16 +61,18 @@ highest = beginnerHighest;
 elseif level == moderate
 highest = moderateHighest;
 
-elseif level == advanced; 
-highest = advancedHighest; 
-%Bug no.3: I found this while running the code. Incorrect spelling of highest
-%used. Corrected the small letter h with capital 'H' in advancedHighest. 
+elseif level == advanced; %Bug no.3 : Conditional for elseif was missing here.
+%Added level == advanced in line 64.
+
+highest = advancedHighest; %Bug no.4: I found this while running the code. 
+% Incorrect spelling of highest used. Corrected the small letter h with 
+% capital 'H' in advancedHighest. 
 
 end
 
 % randomly select secret number between 1 and highest for level of play
 
-%Bug no.4: Although there was no error message displayed while running the
+%Bug no.5: Although there was no error message displayed while running the
 %code for the below line. However, I noticed that, earlier because of the use of 
 % '+1' in the below line of code, the only secretNumber selected by default
 % was the highest number from the respective levels of play. Removing the
@@ -82,7 +82,7 @@ secretNumber = floor(rand() * highest);
 
 % initialize number of guesses and User_guess
 
-%Bug no.5: numOfTries needs to start from 0 or else one additional number
+%Bug no.6: numOfTries needs to start from 0 or else one additional number
 %of tries used is added in the end after the user correctly guesses the
 %number. Therefore, numOfTries value changes from 1 to 0.
 numOfTries = 0; 
@@ -90,7 +90,7 @@ userGuess = 0;
 
 % repeatedly get user's guess until the user guesses correctly
 while userGuess ~= secretNumber
-%end      %%Bug no.6: The presence of end in this line made the game end here. 
+%end      %%Bug no.7: The presence of end in this line made the game end here. 
 % The script after line 93 was not executed wherein the user enters the guess and
 % game moves forward. After commenting the 'end' at line 93, the game went 
 % ahead properly.
@@ -100,10 +100,12 @@ fprintf('\nEnter a guess (1-%d): ', highest);
 userGuess = input('');
 
 % add 1 to the number of guesses the user has made
-numOfTries = numOfTries + 1; % Bug : moved this from bottom to top.
+numOfTries = numOfTries + 1; % Bug no.8 : Moved this line of code from below
+% and included it in this while loop so that the number of tried is added 
+% each time the user guess is wrong.
 
 while userGuess < 1 || userGuess > highest 
-    % Bug no.7: The greater than pr equals highest made the secretNumber 
+    % Bug no.9: The greater than pr equals highest made the secretNumber 
     % (with the highest number) unreachable. Hence, removed the equals sign
     % and replaced >= by >.
 fprintf('Sorry, that is not a valid guess.\nRe-enter a guess (1-%d): ', highest);
@@ -113,8 +115,8 @@ end % of guess validation loop
 
 % report whether the user's guess was high, low, or correct
 
-% Bug no.8: Replaced the > operator in the below line of code with a <
-% operator to satisfy all the either & or conditions for number guesses that
+% Bug no.10: Used appropriate > and < operators in the below lines of code
+% to satisfy all the either & or conditions for number guesses that
 % are less or greater than the secret number.
 if userGuess < secretNumber 
 fprintf('Sorry, %d is too low.\n', userGuess);
@@ -123,7 +125,7 @@ fprintf('Sorry, %d is too high.\n', userGuess);
 elseif numOfTries == 1
 fprintf('\nLucky You!  You got it on your first try!\n\n');
 else
-    %Bug no.9:There was no error message for below line of code
+    %Bug no.11:There was no error message for below line of code
     %while running the script. The end message only showed the secret number
     %that was chosen randomly and not the number of tries used by the user
     %to guess that number. Adding 'numOfTries' in the below line gave that
@@ -135,5 +137,4 @@ fprintf('Game Over. Thanks for playing the Guess That Number game.\n\n');
 
 end  % of guessing while loop
 end 
-end
-% end of game
+end % Bug no. 12: Added the two missing 'end' at the end here to satisfy closing of the appropriate conditionals written above.
